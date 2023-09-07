8e037d895a798e60d955ce0a5a1a9a8e4db25c73
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2023-09-07 18:05:40 +0200

    Login User View with Validation model

---
+++ Blog.Logic/Dto/Validators/LoginUserDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using Blog.Logic.Dto.UserDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Logic.Dto.Validators
+{
+    public class LoginUserDtoValidator : AbstractValidator<LoginUserDto>
+    {
+        public LoginUserDtoValidator()
+        {
+            RuleFor(l => l.Email)
+                .EmailAddress()
+                .WithMessage("Invalid Type");
+
+            RuleFor(l => l.Password)
+                .MinimumLength(8)
+                .WithMessage("Password is too short");
+        }
+    }
+}
