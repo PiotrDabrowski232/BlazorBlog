74dc5527924d0fd556d2f4eb3e32789ea0bd201c
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2023-10-08 17:00:01 +0200

    Tag and tagPosts repositories

---
+++ Blog.Data/Repositories/TagPostsReposiotry.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Data.Data;
+using Blog.Data.Models;
+using Blog.Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Repositories
+{
+    public class TagPostsReposiotry : GenericRepository<TagPosts>, IGenericRepository<TagPosts>, ITagPostsRepository
+    {
+        public TagPostsReposiotry(BlogDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Blog.Data/Repositories/TagReposiotry.cs
@@ -0,0 +1,18 @@
+﻿using Blog.Data.Data;
+using Blog.Data.Models;
+using Blog.Data.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data.Repositories
+{
+    public class TagReposiotry : GenericRepository<Tag>, IGenericRepository<Tag>, ITagRepository
+    {
+        public TagReposiotry(BlogDbContext context) : base(context)
+        {
+        }
+    }
+}
