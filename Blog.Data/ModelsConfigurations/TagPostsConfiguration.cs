eb7e43d66ef9bd9c37dc2651ad7a6ec785a008f5
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2023-10-08 16:28:53 +0200

    entity config for tags

---
+++ Blog.Data/ModelsConfigurations/TagPostsConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.AspNetCore.Identity;
+using System;
+using Blog.Logic.Extensions;
+
+namespace Blog.Data.ModelsConfigurations
+{
+    public class TagPostsConfiguration : IEntityTypeConfiguration<TagPosts>
+    { 
+        public void Configure(EntityTypeBuilder<TagPosts> builder)
+        {
+            builder.HasKey(tp => new { tp.TagId, tp.PostId});
+        }
+    }
+}
