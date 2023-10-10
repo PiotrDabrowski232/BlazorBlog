16c27dc3c7ce4cc9e50f1d0f148deca067a4395b
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2023-10-10 21:28:53 +0200

    Dependecy Injection Config in separate file

---
+++ Blog/Configuration/RepositoryDependencyConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Data.Repositories.Interfaces;
+using Blog.Data.Repositories;
+
+namespace Blog.Configuration
+{
+    public static class RepositoryDependencyConfiguration
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection Services)
+        {
+            Services.AddScoped<IUserRepository, UserRepository>();
+            Services.AddScoped<IRoleRepository, RoleReposiotry>();
+            Services.AddScoped<IPostRepository, PostRepository>();
+            Services.AddScoped<ITagRepository, TagRepository>();
+            Services.AddScoped<ITagPostsRepository, TagPostsRepository>();
+
+            return Services;
+        }
+    }
+}
--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,21 +1,14 @@
-using Blog.Data;
 using Blog.Data.Data;
-using Blog.Logic.Services.Interfaces;
-using Blog.Logic.Services;
 using Microsoft.EntityFrameworkCore;
 using Blog.Data.Repositories.Interfaces;
 using Blog.Data.Repositories;
-using Blog.Data.Models;
-using Microsoft.AspNetCore.Identity;
-using FluentValidation;
 using FluentValidation.AspNetCore;
 using Blog.Logic.Dto.UserDtos;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.AspNetCore.Components.Authorization;
 using Blog.Logic.Authentication;
 using Radzen;
-using Blog.Logic.Dto.Validators.UserValidator;
-using Blog.Logic.Services.BackgroundServices;
+using Blog.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,24 +26,13 @@ builder.Services.AddScoped<UserSession>();
 
 
 // Dependency Injections Reposiotries
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IRoleRepository, RoleReposiotry>();
-builder.Services.AddScoped<IPostRepository, PostRepository>();
-builder.Services.AddScoped<ITagRepository, TagRepository>();
-builder.Services.AddScoped<ITagPostsRepository, TagPostsRepository>();
+builder.Services.AddRepositories();
 
 
 
 
 // Dependency Injections Services
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IRoleService, RoleService>();
-builder.Services.AddScoped<IPostService, PostService>();
-builder.Services.AddScoped<ITagService, TagService>();
-builder.Services.AddScoped<ITagPostsService, TagPostsService>();
-builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
-builder.Services.AddScoped<IValidator<UserDto>, UserDtoValidator>();
-builder.Services.AddHostedService<BackgroundDeleting>();
+builder.Services.AddServices();
 
 
 //DbContext
