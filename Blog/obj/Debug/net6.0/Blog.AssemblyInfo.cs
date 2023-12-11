1c0010e72cd09fb1f63aa8ecdc3f2dd42da994dd
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2023-12-11 15:12:13 +0100

    azure configuration

--- Blog/obj/Debug/net6.0/Blog.AssemblyInfo.cs
+++ Blog/obj/Debug/net6.0/Blog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2da9b9ea39d2cf2c8465ac09c2a83abc044b64c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
