232dbaea8d15ffa90b1368953b51917ca9f7dff4
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2023-12-20 13:51:51 +0100

    azure Function Created

---
+++ VerificationEmailSender/Function1.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace VerificationEmailSender
+{
+    public class Function1
+    {
+        [FunctionName("Function1")]
+        public void Run([QueueTrigger("verificationqueue", Connection = "verificationqueueConnection")]string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        }
+    }
+}
