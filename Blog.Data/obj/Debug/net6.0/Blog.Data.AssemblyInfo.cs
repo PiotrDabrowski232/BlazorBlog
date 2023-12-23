e71d35f99eb416f32358ef2055c08a92e699dc30
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2023-12-23 11:23:39 +0100

    nothing changed

--- Blog.Data/obj/Debug/net6.0/Blog.Data.AssemblyInfo.cs
+++ Blog.Data/obj/Debug/net6.0/Blog.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9ade1a8651c2d5e09288f684271c1d6c0b01095")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
